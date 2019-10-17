ee6fe2e60151fe85429bd19b109d391e520f8cf4
Rami-Slicer <40865625+Rami-Slicer@users.noreply.github.com>
2019-10-16 19:05:19 -0500

    Added UI via GTK

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import json
 strings = json.loads(open("strings.json", "r").read())
 
-for lang in strings:
-    print(lang['country'] + ": " + lang['string'])
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Global Hello World")
+        self.set_default_size(300, 250)
+
+        scrolledwindow = Gtk.ScrolledWindow()
+        scrolledwindow.set_hexpand(True)
+        scrolledwindow.set_vexpand(True)
+        self.box = Gtk.Box(spacing=1, orientation=1)
+        scrolledwindow.add(self.box)
+
+        for lang in strings:
+            self.label = Gtk.Label(label=lang['country']+": "+lang['string'])
+            self.box.pack_start(self.label, True, True, 0)
+        self.add(scrolledwindow)
+
+
+    def on_button_clicked(self, widget):
+        print("Hello World")
+
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
